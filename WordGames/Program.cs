a047d565c2470ad6e01ecab30dd511e05599f00d
Yao2333kkk <2282340201@qq.com>
2022-01-11 11:49:29 +0800

    Update Program.cs

--- WordGames/Program.cs
+++ WordGames/Program.cs
@@ -7,6 +7,9 @@ namespace WordGames
         static void Main(string[] args)
         {
             Console.WriteLine("Hello WorldNMd!");
+            Console.WriteLine("Hello 233333!");
+            Console.WriteLine("Hello hhhhhhhhh!");
+            Console.WriteLine("Hello aaaaaaa!");
         }
     }
 }
